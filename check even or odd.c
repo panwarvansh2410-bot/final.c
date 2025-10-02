3a85cd9487352c7b4acb527ef7758ffac4ba6aab
Vansh <panwarvansh2410@gmail.com>
2025-10-02 12:24:23 +0530

    Add files via upload

---
+++ check even or odd.c
@@ -0,0 +1,77 @@
+// step 3
+         
+           # include <stdio.h>
+            
+int main() {
+
+    int a = 5 ;
+
+    if(a/2==0) {
+        printf("the number  is even\n");
+    }
+
+    else {
+        printf("the number is odd\n");
+    }
+    return 0 ;
+    }    
+
+
+            // step 4 
+
+     
+      # include <stdio.h>
+            
+int main() {
+
+    int num = 5 ;
+
+    if(num/2==0) {
+        printf("the number  is even\n");
+    }
+
+    else {
+        printf("the number is odd\n");
+    }
+    return 0 ;
+    }       
+
+
+             // step 5 
+      
+# include <stdio.h>
+            
+int main() {
+
+    int num ;
+    printf("enter thenumber :");
+    scanf("%d", &num);
+
+    if(num/2==0) {
+        printf("the number is even\n");
+    }
+        else {
+            printf("the number is odd\n");
+        }
+        return 0 ;
+    }    
+
+
+            // step 6
+  
+         # include <stdio.h>
+            
+int main() {
+
+    int num ;
+    printf("enter thenumber :");
+    scanf("%d", &num);
+
+    if(num/2==0) {
+        printf("the number is even\n");
+    }
+        else {
+            printf("the number is odd\n");
+        }
+        return 0 ;   
+                }     
