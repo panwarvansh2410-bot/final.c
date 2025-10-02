6d0f666da5ddf884f24a31c562c7a7fd9d256b1c
Vansh <panwarvansh2410@gmail.com>
2025-10-02 12:21:30 +0530

    Add files via upload

---
+++ two no. equal or not.c
@@ -0,0 +1,85 @@
+                    //step 3 
+         
+                    
+    # include <stdio.h>
+int main(){
+           
+    int a = 5 ;
+    int b = 5 ;
+    if (a==b) {
+        printf("the numbers are equal\n");
+    }
+        else {
+            printf("the numbers are not equal\n");
+        }
+        return 0 ;
+    }    
+
+
+                // step 4 
+       
+                
+              # include <stdio.h>
+int main(){
+           
+    int firstnum = 5 ;
+    int secondnum = 5 ;
+
+    if (firstnum == secondnum) {
+        printf("the numbers are equal\n");
+    }
+        else {
+            printf("the numbers are not equal\n");
+        }
+        return 0 ;
+    }      
+               
+              
+               
+               // step 5
+
+            # include <stdio.h>
+            
+int main(){
+           
+    int firstnum; 
+    printf("enter the firstnum : ");
+    scanf("%d", &firstnum);
+
+    int secondnum; 
+    printf("enter the secondnum :");
+    scanf("%d", &secondnum );
+
+    if (firstnum == secondnum){
+        printf("the numbers are equal\n");
+    }
+    else {
+        printf(" the numbers are not equal");
+    }
+
+}     
+
+
+             //step 6
+    
+             # include <stdio.h>
+            
+int main() {
+           
+    int firstnum , secondnum ;
+    printf("enter the firstnum : ");
+    scanf("%d", &firstnum);
+
+    
+    printf("enter the secondnum :");
+    scanf("%d", &secondnum );
+
+    if (firstnum == secondnum){
+        printf("the numbers are equal\n");
+    }
+    else {
+        printf(" the numbers are not equal");
+    }
+}      
+
+
