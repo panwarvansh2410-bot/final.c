7448342af450804fffe89b913c2af16236559b54
Vansh <panwarvansh2410@gmail.com>
2025-10-02 12:16:30 +0530

    Add files via upload

---
+++ change celcius to farhenhiet.c
@@ -0,0 +1,49 @@
+// step 3
+      
+# include <stdio.h>
+int main(){
+    int c = 35 ;
+    float f = (c*9/5)+32 ;
+
+    printf("%f", f);
+}    
+
+
+              // step 4
+    
+              
+# include <stdio.h>
+int main(){
+    int celcius = 35 ;
+    float fahrenheit = (celcius*9/5)+32;
+
+    printf("temprature in fahrenheit is : %f", fahrenheit);
+}         
+
+
+                    //step 5
+           
+    # include <stdio.h>
+int main(){
+    int celcius;
+    printf("enter the temprature in celcius :");
+    scanf("%d", &celcius);
+    
+    float fahrenheit = (celcius*9/5)+32;
+    printf("temprature in fahrenheit is : %f ", fahrenheit);
+
+}         
+
+
+                    //step 6 
+              
+                    # include <stdio.h>
+int main(){
+    int celcius;
+    printf("enter the temprature in celcius :");
+    scanf("%d", &celcius);
+    
+    float fahrenheit = (celcius*9/5)+32;
+    printf("temprature in fahrenheit is : %f ", fahrenheit);
+
+                }      
