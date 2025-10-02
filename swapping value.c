08648cc2047ea0a30cdcbb35c5e7617fb44470f4
Vansh <panwarvansh2410@gmail.com>
2025-10-02 12:19:17 +0530

    Add files via upload

---
+++ swapping value.c
@@ -0,0 +1,91 @@
+// step 3
+
+              # include <stdio.h>
+            int main() {
+
+                int a= 5;
+                int b = 8 ;
+               int  temp ;
+
+                temp = a ;
+                a = b ;
+                b = temp ;
+
+                printf ("the value of a after swapping : %d\n", a);
+                printf ("the value of b after swapping : %d\n", b);
+
+
+          return 0;      
+            }        
+
+
+                      // step 4 
+
+                     # include <stdio.h>
+            int main() {
+
+                int  firstnum = 5;
+                int secondnum = 8 ;
+               int  temp ;
+
+                temp = firstnum;
+                firstnum = secondnum ;
+                secondnum = temp ;
+
+                printf ("the value of firstnum after swapping : %d\n", firstnum);
+                printf ("the value of secondnum after swapping : %d\n", secondnum);
+
+                return 0 ;
+
+                }        
+
+
+                        // step 5 
+
+                        # include <stdio.h>
+            int main() {
+
+                int firstnum;
+                printf("enter the firstnum :");
+                scanf("%d", &firstnum);
+
+                int secondnum ;
+                printf("enter the secondnum :");
+                scanf("%d", &secondnum );
+
+                int temp ;
+                temp = firstnum;
+                firstnum = secondnum ;
+                secondnum = temp;
+
+                printf ("the value of firstnum after swapping : %d\n", firstnum);
+                printf ("the value of secondnum after swapping : %d\n", secondnum);
+
+                  return 0 ;
+            }    
+
+
+                 // step 6 
+
+                 # include <stdio.h>
+            int main() {
+
+                int firstnum , secondnum ;
+                printf("enter the firstnum :");
+                scanf("%d", &firstnum);
+
+                
+                printf("enter the secondnum :");
+                scanf("%d", &secondnum );
+
+                int temp ;
+                temp = firstnum;
+                firstnum = secondnum ;
+                secondnum = temp;
+
+                printf ("the value of firstnum after swapping : %d\n", firstnum);
+                printf ("the value of secondnum after swapping : %d\n", secondnum);
+
+                  return 0 ;   }
+
+    
