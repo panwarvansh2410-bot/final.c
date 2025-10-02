0f937facb0658c870598d8000cf0273f7ef5790f
Vansh <panwarvansh2410@gmail.com>
2025-10-02 12:23:00 +0530

    Add files via upload

---
+++ greatest of no. .c
@@ -0,0 +1,117 @@
+// step 3
+
+       # include <stdio.h>
+            
+int main() {
+
+    int a = 56;
+    int b = 89;
+    int c = 45;
+    
+    if(a>=b && a>=c) {
+        printf("a is greatest\n");
+    }
+
+    else if (b>=a &&b>=c) {
+        printf("b is greatest\n");
+    }
+    else {
+        printf("c is greatest\n");
+    }
+     return 0 ;
+    }    
+
+
+
+          // step 4
+ 
+          # include <stdio.h>
+            
+int main() {
+
+    int firstnum = 56;
+    int secondnum = 89;
+    int thirdnum = 45;
+    
+    if(firstnum>=secondnum && firstnum >=thirdnum) {
+        printf("firstnum is greatest\n");
+    }
+
+    else if (secondnum>=firstnum && secondnum>=thirdnum) {
+        printf("secondnum is greatest\n");
+    }
+    else {
+        printf("thirdnum is greatest\n");
+    }
+     return 0 ;
+} 
+
+
+           //step 5
+
+        # include <stdio.h>
+            
+int main() {
+
+    int firstnum ;
+    printf("enter the first number:") ;
+    scanf("%d", &firstnum);
+    
+    int secondnum ;
+    printf("enter the second number :");
+    scanf("%d", &secondnum);
+
+    int thirdnum ;
+    printf("enter the third number :");
+    scanf("%d", &thirdnum);
+
+    if(firstnum>=secondnum && firstnum >=thirdnum) {
+        printf("firstnum is greatest\n");
+    }
+
+    else if (secondnum>=firstnum && secondnum>=thirdnum) {
+        printf("secondnum is greatest\n");
+    }
+    else {
+        printf("thirdnum is greatest\n");
+    }
+     return 0 ;
+
+}    
+
+
+              // step 6
+             
+            
+              
+          # include <stdio.h>
+            
+int main() {
+
+    int firstnum , secondnum , thirdnum ;  
+
+    printf("enter the first number:") ;
+    scanf("%d", &firstnum);
+    
+    
+    printf("enter the second number :");
+    scanf("%d", &secondnum);
+
+    
+    printf("enter the third number :");
+    scanf("%d", &thirdnum);
+
+    if(firstnum>=secondnum && firstnum >=thirdnum) {
+        printf("firstnum is greatest\n");
+    }
+
+    else if (secondnum>=firstnum && secondnum>=thirdnum) {
+        printf("secondnum is greatest\n");
+    }
+    else {
+        printf("thirdnum is greatest\n");
+    }
+     return 0 ;
+}
+      
+
