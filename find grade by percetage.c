d3b09aa17c845539f40a301dcbf5ff704b871bbe
Vansh <panwarvansh2410@gmail.com>
2025-10-02 12:28:11 +0530

    Add files via upload

---
+++ find grade by percetage.c
@@ -0,0 +1,172 @@
+               // step 3 
+
+               # include <stdio.h>
+
+int main() {
+
+    int p = 82;
+    int c = 94;
+    int m = 76;
+    int e = 77;
+    int h = 83;
+
+    int sum  = p + c + m + e + h;
+    float per = (sum )/500.0 * 100 ;
+
+    if(per>= 90){
+        printf("grade A \n");
+    }
+
+    if(per>=80 && per<90) {
+        printf("grade B \n");
+    }
+
+    if(per>=60 && per<80) {
+        printf("grade C\n");
+    }
+
+    if(per<60) {
+        printf("grade D \n");
+    }
+
+    return 0 ;
+
+}       
+
+    
+                // step 4
+     
+         # include <stdio.h>
+
+int main() {
+
+    int physics = 82;
+    int chemistry = 94;
+    int mathematics = 76;
+    int english = 77;
+    int hindi = 83;
+
+    int sum  = physics + chemistry + mathematics + english + hindi;
+    float per = (sum )/500.0 * 100 ;
+
+    if(per>= 90){
+        printf("grade A \n");
+    }
+
+    if(per>=80 && per<90) {
+        printf("grade B \n");
+    }
+
+    if(per>=60 && per<80) {
+        printf("grade C\n");
+    }
+
+    if(per<60) {
+        printf("grade D \n");
+    }
+
+    return 0 ;
+
+ }   
+
+
+                 // step 5
+                 # include <stdio.h>
+
+int main() {
+
+    int physics ;
+    printf("enter the marks of physics :");
+    scanf("%d", &physics);
+
+      int mathematics;
+    printf("enter the marks of mathematics :");
+    scanf("%d", &mathematics);
+
+      int chemistry ;
+    printf("enter the marks of chemistry :");
+    scanf("%d", &chemistry);
+
+        int hindi ;
+    printf("enter the marks of hindi:");
+    scanf("%d", &hindi);
+    
+      int english ;
+    printf("enter the marks of english :");
+    scanf("%d", &english);
+
+
+    int sum  = physics + chemistry + mathematics + english + hindi;
+    float per = (sum )/500.0 * 100 ;
+
+    if(per>= 90){
+        printf("grade A \n");
+    }
+
+    if(per>=80 && per<90) {
+        printf("grade B \n");
+    }
+
+    if(per>=60 && per<80) {
+        printf("grade C\n");
+    }
+
+    if(per<60) {
+        printf("grade D \n");
+    }
+
+    return 0 ;
+}
+    
+    
+       
+
+
+             // step 6 
+
+             # include <stdio.h>
+
+int main() {
+
+    int physics, chemistry, mathematics, hindi, english;
+    printf("enter the marks of physics :");
+    scanf("%d", &physics);
+
+      
+    printf("enter the marks of mathematics :");
+    scanf("%d", &mathematics);
+
+      
+    printf("enter the marks of chemistry :");
+    scanf("%d", &chemistry);
+
+        
+    printf("enter the marks of hindi:");
+    scanf("%d", &hindi);
+    
+      
+    printf("enter the marks of english :");
+    scanf("%d", &english);
+
+
+    int sum  = physics + chemistry + mathematics + english + hindi;
+    float per = (sum )/500.0 * 100 ;
+
+    if(per>= 90){
+        printf("grade A \n");
+    }
+
+    if(per>=80 && per<90) {
+        printf("grade B \n");
+    }
+
+    if(per>=60 && per<80) {
+        printf("grade C\n");
+    }
+
+    if(per<60) {
+        printf("grade D \n");
+    }
+
+    
+}     
