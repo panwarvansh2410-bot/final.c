2dea4f24c5f84bd56084436713dff5bcc3fc7aa1
Vansh <panwarvansh2410@gmail.com>
2025-10-02 12:14:51 +0530

    Add files via upload

---
+++ area circumference of circle.c
@@ -0,0 +1,71 @@
+// step 3
+ 
+# include <stdio.h>
+
+int main() {
+    int r= 14 ;
+    float a = 3.14*r*r ;
+    printf("%f\n", a);
+
+    float c = 2*3.14*r;
+    printf("%f\n", c); 
+
+    return 0 ;
+}
+      
+
+
+
+// step 4 
+    
+# include <stdio.h>
+     
+
+int main() {
+    int radius = 14 ;
+    float area = 3.14*radius*radius ;
+    printf("%f\n", area);
+
+    float circumference = 2*3.14*radius;
+    printf("%f\n", circumference); 
+
+    return 0 ;
+    
+}    
+
+
+// step 5 
+    
+# include <stdio.h>
+
+int main() {
+    int radius ;
+    printf("enter the radius of circle :");
+    scanf("%d", &radius);
+
+    float area = 3.14*radius*radius;
+    printf("area of circle is : %f\n", area);
+
+    float circumference = 2*3.14*radius;
+    printf("circumference of circle is : %f\n", circumference);
+
+}  
+
+
+// step 6
+      
+# include <stdio.h>
+
+int main() {
+
+    int radius ;
+    printf("enter the radius of circle :");
+    scanf("%d", &radius);
+
+    float area = 3.14*radius*radius;
+    printf("area of circle is : %f\n", area);
+
+    float circumference = 2*3.14*radius;
+    printf("circumference of circle is : %f\n", circumference);
+
+}      
