273485ff85fbfe5e226f4413460bf43d7b68c3a7
Vansh <panwarvansh2410@gmail.com>
2025-10-02 12:26:46 +0530

    Add files via upload

---
+++ leap year or not.c
@@ -0,0 +1,74 @@
+// step 3
+                
+                 # include <stdio.h>
+            
+int main() {
+
+    int y = 2025;
+    if(y/4==0) {
+        printf("the year is leap year\n");
+    }
+
+    else {
+        printf("the year is non leap year ");
+    }
+    return 0 ;
+    }    
+
+            //step 4
+                
+
+            # include <stdio.h>
+            int main() {
+
+            int year = 2025;
+    if(year/4==0) {
+        printf("the year is leap year\n");
+    }
+
+    else {
+        printf("the year is non leap year ");
+    }
+    return 0 ;
+    }      
+   
+    
+
+                // step 5 
+                
+        # include <stdio.h>
+            int main() {
+
+                int year ; 
+                printf("enter the year :");
+                scanf("%d", &year);
+
+    if(year/4==0) {
+        printf("the year is leap year\n");
+    }
+    else {
+        printf("the year is non leap year ");
+    }
+    return 0 ;
+}       
+
+
+
+                // step 6
+
+                
+        # include <stdio.h>
+            int main() {
+
+                int year ; 
+                printf("enter the year :");
+                scanf("%d", &year);
+
+    if(year/4==0) {
+        printf("the year is leap year\n");
+    }
+    else {
+        printf("the year is non leap year ");
+    }
+    return 0 ;
+}          
