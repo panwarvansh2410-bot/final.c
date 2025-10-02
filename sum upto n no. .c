b0ffba67906ff80e5b925e0c15fea29aa9ad8fbc
Vansh <panwarvansh2410@gmail.com>
2025-10-02 12:31:43 +0530

    Add files via upload

---
+++ sum upto n no. .c
@@ -0,0 +1,72 @@
+// step 3
+
+# include <stdio.h>
+int main() {
+
+    int n ; 
+    int sum ;
+
+    n = 5;
+    sum = n*(n+1)/2;
+    
+
+        printf("%d", sum);
+     
+        return 0 ;
+
+}    
+
+      // step 4
+
+# include <stdio.h>
+int main() {
+
+    int n ; 
+    int sum ;            //n is first n natural numbers
+
+    n = 5;
+    sum = n*(n+1)/2;
+    
+
+        printf("%d", sum);
+     
+        return 0 ;
+}    
+        
+      // step 5 
+
+# include <stdio.h>
+int main() {
+
+    int n ; 
+    int sum ;
+                                        //n is first n natural numbers
+    printf("enter the value of n :");
+    scanf("%d", &n);
+
+    sum = n*(n+1)/2;
+
+    printf("%d",sum);
+
+    return 0;
+
+}    
+
+         // step 6 
+
+   # include <stdio.h>
+int main() {
+
+    int n , sum ;
+printf("enter the value of n :");
+scanf("%d", &n);
+
+sum = n*(n+1)/2;
+printf( "%d", sum);
+
+return 0; 
+}   
+
+
+
+ 
